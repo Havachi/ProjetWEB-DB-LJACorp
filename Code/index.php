327b418ae9bceed6e59ce65cbc0791244a33b791
Louis Richard <louis.richard@tutanota.com>
2019-05-03 09:16:27 +0200

    Add function register

--- Code/index.php
+++ Code/index.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Author   : louis.richard@cpnv.ch
- * Project  : ProjetWEBDB
+ * Author   : nicolas.glassey@cpnv.ch
+ * Project  : ProjetWeb + BDD
  * Created  : 09.04.2019 - 13:46
  *
- * Last update :    [03.05.2019 author]
- *
- * Git source  :    [https://github.com/Havachi/ProjetWEB-DB-LJACorp]
+ * Last update :    [03.105.2019 louis.richard@cpnv.ch]
+ *                  [Add case login]
+ * Git source  :    [link]
  */
 
 session_start();
@@ -17,6 +17,12 @@ if (isset($_GET['action'])) {
     switch ($action) {
         case 'home' :
             home();
+            break;
+        case 'register' :
+            register($_POST);
+            break;
+        case 'login' :
+            login($_POST);
             break;
         default :
             home();
