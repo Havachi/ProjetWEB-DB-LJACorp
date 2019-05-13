0a2e86c98106c6fc03fce1407344187d8ad63652
Alessandro ROSSI <35927984+Havachi@users.noreply.github.com>
2019-05-13 09:31:40 +0200

    Added negative number verification
    Type: model

--- Code/model/cartManager.php
+++ Code/model/cartManager.php
@@ -19,29 +19,35 @@
  */
 
 function updateCart($currentCartArray, $snowCodeToAdd, $qtyOfSnowsToAdd, $howManyLeasingDays){
-    //reset the Updated Cart
-    $alreadyExist=false;
-    $cartUpdated = array();
-    if($currentCartArray != null){
-        $cartUpdated = $currentCartArray;
-    }
-    foreach ($currentCartArray as $key => &$cart){
-        if($snowCodeToAdd == $cart['code']){
-            if ($howManyLeasingDays == $cart['nbD']){
-                $tempqty = $cart['qty'];
-                $cart['qty'] = $tempqty + $qtyOfSnowsToAdd;
-                $alreadyExist=true;
+    //Doesn't let the user user value under 1
+    if ($qtyOfSnowsToAdd > 0 and $howManyLeasingDays > 0) {
+        $alreadyExist = false;
+        $cartUpdated = array();
+        if ($currentCartArray != null) {
+            $cartUpdated = $currentCartArray;
+        }
+        //Verification if the code and leasing days are the some on one of every item in cart
+        foreach ($currentCartArray as $key => &$cart) {
+            if ($snowCodeToAdd == $cart['code']) {
+                if ($howManyLeasingDays == $cart['nbD']) {
+                    $tempqty = $cart['qty'];
+                    $cart['qty'] = $tempqty + $qtyOfSnowsToAdd;
+                    $alreadyExist = true;
 
+                }
             }
         }
-    }
-    if(!$alreadyExist){
-        $newSnowLeasing = array('code' => $snowCodeToAdd, 'dateD' => Date("d-m-y"), 'qty' => $qtyOfSnowsToAdd, 'nbD' => $howManyLeasingDays);
-        array_push($cartUpdated, $newSnowLeasing);
+        if (!$alreadyExist) {
+            $newSnowLeasing = array('code' => $snowCodeToAdd, 'dateD' => Date("d-m-y"), 'qty' => $qtyOfSnowsToAdd, 'nbD' => $howManyLeasingDays);
+            array_push($cartUpdated, $newSnowLeasing);
+        } else {
+            $cartUpdated = $currentCartArray;
+        }
+        return $cartUpdated;
     }else{
-        $cartUpdated = $currentCartArray;
+        //error message
+        echo "Quantité trop élevée ou inférieure à 1, Vérifiez la disponibilité du stock";
     }
-    return $cartUpdated;
 }
 
 //in_array https://www.php.net/manual/en/function.in-array.php
