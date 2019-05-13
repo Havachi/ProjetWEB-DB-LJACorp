468d310a6ac0c33d7def1c834b297332a67f2f12
Alessandro ROSSI <35927984+Havachi@users.noreply.github.com>
2019-05-13 12:16:40 +0200

    Comment Template

---
+++ Code/templates/Comment.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *|File Info|
+ *
+ *   /-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-\
+ *  | Author   :                                                                         |
+ *  | Project  : ProjetWEB-DB-LJACorp                                                    |
+ *  | Created  : [DATE] - [TIME]                                                         |
+ *  |                                                                                    |
+ *  | Last update :    [DATE]                                                            |
+ *  |                                                                                    |
+ *  | Git source  :    [https://github.com/Havachi/ProjetWEB-DB-LJACorp]                 |
+ *   \-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-/
+ */
