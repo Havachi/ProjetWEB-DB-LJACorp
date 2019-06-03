ef127b69154a12ff5e44ae6e519bf37650b47aba
JonasHau <35954302+JonasHau@users.noreply.github.com>
2019-06-03 11:59:58 +0200

    Update leasing.php
    added intro text and began table

--- Code/view/leasing.php
+++ Code/view/leasing.php
@@ -5,6 +5,7 @@
  * Date: 			27.05.2019
  * Last Update :    Jonas.HAUTIER
  *					27.05.2019 - creation du fichier et update de l'entete
+ *					03.06.2019 - ajout du tableau de location
  */
 
 $title = 'Rent A Snow - Vos locations';
@@ -12,7 +13,20 @@ $title = 'Rent A Snow - Vos locations';
 ob_start();
 ?>
 	<h2>Vos Locations</h2>
-	
+	<article>
+		<!-- intro text -->
+		<div>
+			<p>Votre demande de location a été enregistrée.</p></br>
+			<p>Si vous souhaitez visualiser votre commande, cliquez sur le logo <a href="">PDF</a></p>
+		</div>
+		
+		<!-- leasing table -->
+		<form method="POST" action="">
+			<table class="table">
+			
+			</table>
+		</form>
+	</article>
 	
 <?php
 $content = ob_get_clean();
