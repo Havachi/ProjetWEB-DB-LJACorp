e85c221c2bc4ee3af4af99d37b9f5f35bb7a9962
JonasHau <35954302+JonasHau@users.noreply.github.com>
2019-06-04 20:23:49 +0200

    Leasing.php is finished

--- Code/view/leasing.php
+++ Code/view/leasing.php
@@ -6,6 +6,7 @@
  * Last Update :    Jonas.HAUTIER
  *					27.05.2019 - creation du fichier et update de l'entete
  *					03.06.2019 - ajout du tableau de location
+ *					04.06.2019 - fini... a verifier sur l instance
  */
 
 $title = 'Rent A Snow - Vos locations';
@@ -19,15 +20,35 @@ ob_start();
 			<p>Votre demande de location a été enregistrée.</p></br>
 			<p>Si vous souhaitez visualiser votre commande, cliquez sur le logo <a href="">PDF</a></p>
 		</div>
-		
+
 		<!-- leasing table -->
-		<form method="POST" action="">
+		<form>
 			<table class="table">
-			
+				<tr>
+        			<th>N° de location</th>
+					<th>Code</th>
+					<th>Marque</th>
+					<th>Modèle</th>
+					<th>Prix</th>
+					<th>Quantité</th>
+					<th>Date début de location</th>
+				</tr>
+				<tr>
+					<?php
+					foreach	($leasing as $leasRow) : ?>
+							<td><?= $leasRow['IDLoc']; ?></td>
+							<td><?= $leasRow['snowCode']; ?></td>
+							<td><?= $leasRow['snowBrand']; ?></td>
+							<td><?= $leasRow['snowModel']; ?></td>
+							<td><?= $leasRow['dailyPrice']; ?></td>
+							<td><?= $leasRow['qtyLoc']; ?></td>
+							<td><?= $leasRow['dateLoc']; ?></td>
+					<?php endforeach; ?>
+				</tr>
 			</table>
 		</form>
 	</article>
-	
+
 <?php
 $content = ob_get_clean();
 require 'gabarit.php';
