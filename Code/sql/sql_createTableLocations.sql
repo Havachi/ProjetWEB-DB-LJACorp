a72043b920b787a8af55ae130dcf2c5c7a6ef277
Alessandro ROSSI <35927984+Havachi@users.noreply.github.com>
2019-06-07 09:31:14 +0200

    Changed SQL script for table location

--- Code/sql/sql_createTableLocations.sql
+++ Code/sql/sql_createTableLocations.sql
@@ -1,12 +1,28 @@
-CREATE TABLE Locations(
-  IDLoc INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  FKUser INT(10) UNSIGNED NOT NULL,
-  FKSnow INT(11) NOT NULL,
-  DateLoc DATE,
-  QtyLoc SMALLINT,
-  NbDLoc SMALLINT,
-  PRIMARY KEY (`IDLoc`),
-  FOREIGN KEY (FKUser) REFERENCES users(IDUser),
-  FOREIGN KEY (FKSnow) REFERENCES snows(IDSnow)
-) COLLATE = 'utf8_general_ci' ENGINE = InnoDB;
+DROP TABLE IF EXISTS `Locations`;
+CREATE TABLE `Locations`
+(
+    IDLoc        INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    FK_IDUser    INT(10) UNSIGNED NOT NULL,
+    DateLocStart DATE,
+    DateLocEnd   DATE,
+    LocStatus    smallint(2),
+    PRIMARY KEY (`IDLoc`),
+    FOREIGN KEY (FK_IDUser) REFERENCES users (IDUser)
+) COLLATE = 'utf8_general_ci'
+  ENGINE = InnoDB;
 
+DROP TABLE IF EXISTS `OrderedSnow`;
+CREATE TABLE `OrderedSnow`
+(
+    IDOrder        INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    FK_IDLoc       INT(10) UNSIGNED NOT NULL,
+    FK_IDSnow      INT(11) UNSIGNED NOT NULL,
+    DateOrderEnd   DATE,
+    QtyOrder       SMALLINT(6),
+    NbDOrder       SMALLINT(6),
+    OrderStatus    SMALLINT(2),
+    PRIMARY KEY (`IDOrder`),
+    FOREIGN KEY (FK_IDLoc) REFERENCES users (IDUser),
+    FOREIGN KEY (FK_IDSnow) REFERENCES snows (IDSnow)
+) COLLATE = 'utf8_general_ci'
+  ENGINE = InnoDB;
