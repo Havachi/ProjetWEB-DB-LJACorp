b9ea483c55f81d3ceb8b2133ce78d449715ef98e
JonasHau <35954302+JonasHau@users.noreply.github.com>
2019-06-07 08:33:50 +0200

    Update leasingSeller.php
    Began working on this page

--- Code/view/leasingSeller.php
+++ Code/view/leasingSeller.php
@@ -6,13 +6,37 @@
  * Last Update :    Jonas.HAUTIER
  *					27.05.2019 - creation du fichier et de l'entete
  */
- 
+
 $title = 'Rent A Snow - Vos locations (vendeur)';
 
 ob_start();
 ?>
-
-
+	<!-- title -->
+	<h2>Locations en cours</h2>
+	<article>
+		<!-- leasing table -->
+		<form>
+			<table class="table">
+				<tr>
+          <th>Location</th> <!-- id du snow + redirect to details -->
+					<th>Client</th> <!-- email client -->
+					<th>Début de location</th> <!-- date -->
+					<th>Fin de location</th> <!-- date -->
+					<th>Statut</th> <!-- En cours/Rendu partiel/Rendu -->
+				</tr>
+				<tr>
+					<?php
+					foreach	($leasing as $leasRow) : ?>
+							<td><?= $leasRow['IDLoc']; ?></td>
+							<td><?= $leasRow['']; ?></td>
+							<td><?= $leasRow['']; ?></td>
+							<td><?= $leasRow['']; ?></td>
+							<td><?= $leasRow['dateLoc']; ?></td>
+					<?php endforeach; ?>
+				</tr>
+			</table>
+		</form>
+	</article>
 
 <?php
 $content = ob_get_clean();
